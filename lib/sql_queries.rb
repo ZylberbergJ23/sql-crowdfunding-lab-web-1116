555881e13a90770504000382728583ff38f1f38d
ZylberbergJ23 <Zylberberg.Jonathan@gmail.com>
2016-12-08 19:42:52 -0500

    Update sql_queries.rb

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -17,7 +17,8 @@ JOIN pledges ON pledges.user_id = users.id GROUP BY users.name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) FROM projects JOIN pledges ON pledges.project_id = projects.id GROUP BY projects.title
+"SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) FROM projects JOIN pledges ON pledges.project_id = projects.id 
+GROUP BY projects.title
 HAVING SUM(pledges.amount) >= projects.funding_goal"
 end
 
